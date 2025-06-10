3ea549e2800e330e74a6e941d1d10ea781301cf2
DuncanAngel <DuncanAngel070@gmail.com>
2025-06-10 10:14:50 +0200

    Made add form for products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,13 +4,35 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Products;
+use App\Form\ProductForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
+    #[Route('/product/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Products();
+        $form = $this->createForm(ProductForm::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home', ['id' => $product->getId()]);
+        }
+
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView(),
+            'categories' => $categories,
+        ]);
+    }
     #[Route('/product/{id}', name: 'app_product')]
     public function index(int $id, EntityManagerInterface $entitymanager): Response
     {
---
+++ src/Form/ProductForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Brand;
+use App\Entity\Build;
+use App\Entity\Category;
+use App\Entity\Products;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('stock')
+            ->add('description')
+            ->add('image', FileType::class, [])
+            ->add('brand', EntityType::class, [
+                'class' => Brand::class,
+                'choice_label' => 'name',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
