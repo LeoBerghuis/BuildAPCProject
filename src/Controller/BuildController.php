1cdbdb711bc6bddc2fddcaceb7a09480293ad270
LeoBerghuis <leoberghuis@icloud.com>
2025-06-11 12:39:19 +0200

    Cart start

--- src/Controller/BuildController.php
+++ src/Controller/BuildController.php
@@ -5,26 +5,72 @@ namespace App\Controller;
 use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
 
 final class BuildController extends AbstractController
 {
     #[Route('/build', name: 'app_build')]
-    public function index(ProductRepository $ProductRepository, EntityManagerInterface $entitymanager): Response
+    public function index(
+        Request $request,
+        ProductRepository $productRepository,
+        EntityManagerInterface $entityManager,
+        SessionInterface $session
+    ): Response
     {
-        $categories = $entitymanager->getRepository(Category::class)->findAll();
-        return $this->render('build/index.html.twig', [
-            'cpus' => $ProductRepository->findBy(['category' => 1]),
-            'gpu' => $ProductRepository->findBy(['category' => 2]),
-            'motherboards' => $ProductRepository->findBy(['category' => 3]),
-            'rams' => $ProductRepository->findBy(['category' => 4]),
-            'memory' => $ProductRepository->findBy(['category' => 5]),
-            'powersupply' => $ProductRepository->findBy(['category' => 6]),
-            'case' => $ProductRepository->findBy(['category' => 7]),
-            'categories' => $categories,
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        if ($request->isMethod('POST')) {
+            $session->set('pc_build', [
+                'cpu' => $request->request->get('cpu'),
+                'gpu' => $request->request->get('gpu'),
+                'motherboard' => $request->request->get('motherboard'),
+                'ram' => $request->request->get('ram'),
+                'memory' => $request->request->get('memory'),
+                'powersupply' => $request->request->get('powersupply'),
+                'case' => $request->request->get('case'),
+            ]);
 
+            return $this->redirectToRoute('app_cart');
+        }
+
+        return $this->render('build/index.html.twig', [
+            'cpus' => $productRepository->findBy(['category' => 1]),
+            'gpu' => $productRepository->findBy(['category' => 2]),
+            'motherboards' => $productRepository->findBy(['category' => 3]),
+            'rams' => $productRepository->findBy(['category' => 4]),
+            'memory' => $productRepository->findBy(['category' => 5]),
+            'powersupply' => $productRepository->findBy(['category' => 6]),
+            'case' => $productRepository->findBy(['category' => 7]),
+            'categories' => $categories,
         ]);
+    }
+
+    #[Route('/cart', name: 'app_cart')]
+    public function cart(EntityManagerInterface $entityManager,SessionInterface $session, ProductRepository $productRepository): Response
+    {
+        $build = $session->get('pc_build', []);
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        $products = [];
+        foreach ($build as $component => $productId) {
+            $product = $productRepository->find($productId);
+            if ($product) {
+                $products[$component] = $product;
+            }
+        }
+
+        return $this->render('build/cart.html.twig', [
+            'build' => $products,
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/cart-remove', name: 'app_cart_remove')]
+    public function cartRemove(SessionInterface $session): Response
+    {
+        $session->remove('pc_build');
+        return $this->redirectToRoute('app_build');
     }
 }
